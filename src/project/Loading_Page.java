5f78e47227312db37d6fe4cfb5db726617fdb878
SapphireFang2000 <n.a.refat2000@gmail.com>
2022-02-26 01:57:17 +0600

    Loading Page Code

--- src/project/Loading_Page.java
+++ src/project/Loading_Page.java
@@ -9,12 +9,14 @@ package project;
  *
  * @author Sapphire Fang
  */
-public class Loading_Page extends javax.swing.JFrame {
+public class Loading_Page extends javax.swing.JFrame 
+{
 
     /**
      * Creates new form Loading_Page
      */
-    public Loading_Page() {
+    public Loading_Page() 
+    {
         initComponents();
     }
 
@@ -28,7 +30,7 @@ public class Loading_Page extends javax.swing.JFrame {
     private void initComponents() {
 
         jPanel1 = new javax.swing.JPanel();
-        parcentage = new javax.swing.JLabel();
+        percentage = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
         progressBar = new javax.swing.JProgressBar();
         jLabel3 = new javax.swing.JLabel();
@@ -40,9 +42,9 @@ public class Loading_Page extends javax.swing.JFrame {
 
         jPanel1.setBackground(new java.awt.Color(255, 153, 51));
 
-        parcentage.setFont(new java.awt.Font("Monotype Corsiva", 1, 48)); // NOI18N
-        parcentage.setForeground(new java.awt.Color(255, 255, 255));
-        parcentage.setText("%");
+        percentage.setFont(new java.awt.Font("Monotype Corsiva", 1, 48)); // NOI18N
+        percentage.setForeground(new java.awt.Color(255, 255, 255));
+        percentage.setText("%");
 
         jLabel2.setFont(new java.awt.Font("Monotype Corsiva", 1, 36)); // NOI18N
         jLabel2.setForeground(new java.awt.Color(255, 255, 255));
@@ -53,8 +55,6 @@ public class Loading_Page extends javax.swing.JFrame {
         jLabel3.setFont(new java.awt.Font("Monotype Corsiva", 1, 36)); // NOI18N
         jLabel3.setForeground(new java.awt.Color(255, 255, 255));
         jLabel3.setText("Hyacinth");
-
-        jLabel4.setIcon(new javax.swing.ImageIcon("D:\\Java Program Documents\\Project\\Images\\Pharmacy_Logo-removebg-preview.png")); // NOI18N
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -68,11 +68,11 @@ public class Loading_Page extends javax.swing.JFrame {
                         .addComponent(jLabel2)
                         .addGap(108, 108, 108))
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                        .addComponent(parcentage, javax.swing.GroupLayout.PREFERRED_SIZE, 48, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(297, 297, 297))
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
                         .addComponent(jLabel4, javax.swing.GroupLayout.PREFERRED_SIZE, 134, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(274, 274, 274))))
+                        .addGap(274, 274, 274))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                        .addComponent(percentage, javax.swing.GroupLayout.PREFERRED_SIZE, 134, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(239, 239, 239))))
             .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                 .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
                     .addContainerGap(285, Short.MAX_VALUE)
@@ -87,7 +87,7 @@ public class Loading_Page extends javax.swing.JFrame {
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(jLabel4, javax.swing.GroupLayout.PREFERRED_SIZE, 129, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 71, Short.MAX_VALUE)
-                .addComponent(parcentage, javax.swing.GroupLayout.PREFERRED_SIZE, 65, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(percentage, javax.swing.GroupLayout.PREFERRED_SIZE, 65, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(progressBar, javax.swing.GroupLayout.PREFERRED_SIZE, 20, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap())
@@ -116,36 +116,27 @@ public class Loading_Page extends javax.swing.JFrame {
     /**
      * @param args the command line arguments
      */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
+    public static void main(String args[]) 
+    {
+        Loading_Page loadingPage = new Loading_Page();
+        loadingPage.setVisible(true);
+        
+        try
+        {
+            for (int i = 0; i <= 100; i++) 
+            {
+                Thread.sleep(40);
+                loadingPage.progressBar.setValue(i);
+                loadingPage.percentage.setText(Integer.toString(i)+"%");
             }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
-        //</editor-fold>
-
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new Loading_Page().setVisible(true);
-            }
-        });
+        catch (Exception e)
+        {
+            
+        }
+        
+        new Login().setVisible(true);
+        loadingPage.dispose();
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
@@ -153,7 +144,7 @@ public class Loading_Page extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
     private javax.swing.JPanel jPanel1;
-    private javax.swing.JLabel parcentage;
+    private javax.swing.JLabel percentage;
     private javax.swing.JProgressBar progressBar;
     // End of variables declaration//GEN-END:variables
 }
